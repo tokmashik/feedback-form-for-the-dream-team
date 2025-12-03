97b32fe6f8013134387b8104bb03429610cb5953
Лиза Токмашова <tokmashik@MacBook-Air-Liza.local>
2025-12-03 22:42:33 +0700

    fix: add error screen

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,24 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+  import { computed } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
 
-import ModalWrapper from '@/components/ModalWrapper.vue';
-import FeedbackForm from '@/components/FeedbackForm.vue';
-import SuccessForm from '@/components/SuccessForm.vue';
-import ErrorScreen from '@/components/ErrorScreen.vue';
+  import ModalWrapper from '@/components/ModalWrapper.vue';
+  import FeedbackForm from '@/components/FeedbackForm.vue';
+  import SuccessForm from '@/components/SuccessForm.vue';
+  import ErrorScreen from '@/components/ErrorScreen.vue';
 
-const route = useRoute();
-const router = useRouter();
+  const route = useRoute();
+  const router = useRouter();
 
-const currentRoute = computed(() => route.path);
+  const currentRoute = computed(() => route.path);
 
-const goHome = () => router.push('/');
+  const goHome = () => router.push('/');
 
-// Так как нам нужно отобразить 2 вида формы, то рандомно просто показываем одну из них
-const goRandom = () => {
-  const isSuccess = Math.random() > 0.5;
-  router.push(isSuccess ? '/success' : '/error');
-};
+  // Так как нам нужно отобразить 2 вида формы, то рандомно просто показываем одну из них
+  const goRandom = () => {
+    const isSuccess = Math.random() > 0.5;
+    router.push(isSuccess ? '/success' : '/error');
+  };
 </script>
